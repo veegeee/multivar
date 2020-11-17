123c4dbec2ecdf532b0a8bc1951b2368a77bfec9
veegeee <sander_8_2@hotmail.com>
2020-11-17 20:11:02 +0100

    analysis update

--- analysis.R
+++ analysis.R
@@ -1,4 +1,5 @@
 model1<-lm(dat$exam~dat$sex+dat$age+dat$confid+dat$depress+dat$fost)
 #age and confidence are significant predictors for exam score
 #depression, sex and fear are not
+mean(dat$exam)
 summary(model1)

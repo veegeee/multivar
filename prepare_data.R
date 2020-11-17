9366e56c9ce78f6bd6faa2653d023f273ea23c33
veegeee <sander_8_2@hotmail.com>
2020-11-17 19:55:24 +0100

    prepare data update

--- prepare_data.R
+++ prepare_data.R
@@ -4,3 +4,11 @@
 # to store the data.
 
 library(worcs)
+library("foreign")
+dat <- read.spss(file = "FEARadapted.sav", use.value.labels = FALSE, as.data.frame = TRUE, use.missings=TRUE)
+is.data.frame(dat)
+dat<-as.data.frame(dat)
+str(dat)
+dat$group <- as.factor(dat$group)
+dat$sex <- as.factor(dat$sex)
+open_data(dat)
